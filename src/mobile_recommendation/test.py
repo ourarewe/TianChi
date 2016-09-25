4392b9de5b5930720c0233c988b555c53b4dfd9e
ourarewe <489541930@qq.com>
2016-09-25 15:17:45 +0800

    Change-Id: Ibc1981d7f31d621f8a660e8249e892286ccab5a7 Signed-off-by: ourarewe <489541930@qq.com>

--- src/mobile_recommendation/test.py
+++ src/mobile_recommendation/test.py
@@ -2,21 +2,37 @@
 import numpy as np
 from numpy import *
 import pandas as pd
+from sklearn import preprocessing
+
 
 print 'start'
 
 #print (85241/1.0)/(714*1.0)
-print (201337/19.0)/(650*2.0)
+print (61253/1.0)/(714*1.0)
+
+F1 = 6.71755725/100.0; P = 0.05548550 ;R = F1*P/(2*P-F1)
+print  '预测对了',528*R,'个'
+
+F1 = 5.29363110/100.0; P = 0.04624277 ;R = F1*P/(2*P-F1)
+print  '预测对了',528*R,'个'
+
+p = 0.055; r = 44/528.0
+print 'F1 =',2.0*p*r/(p+r)
 
 '''
-df = pd.read_csv('D:\\Documents\\SQL\\tianchi\\mobile_recommendation\\train.csv')
+df = pd.read_csv('D:\\Documents\\SQL\\tianchi\\mobile_recommendation\\train.csv', header=None)
 df = df.fillna(value=0)
 #print df.head()
 X = df.iloc[:,2:14].values
-y = df['label'].values
+y = df.iloc[:,-1].values
 '''
 
-
+'''
+a = array([[1,2],[3,4]])
+b = [[1],[0]]
+print sum(b,1)==0
+print a[sum(b,1)==0]
+'''
 
 
 print 'finished'
